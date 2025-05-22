cb03d72ff5e6bdbce86d5ed437fb6e50a1ae4d34
itigra5 <ilhem.mezaache@gmail.com>
2025-05-22 22:32:49 +0200

    premiere modif pour changer en react : les categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import CatSection from "./Componant/CategoriesSection";
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+function App(){
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div id="body">
+        <div id="Categories">
+        <CatSection
+            titre="Pâtisserie"
+            lien="/test.html"
+            image="/public/images/img_patisserie_cat.jpg"
+            id="ClassPatsry"
+  />
+  <CatSection
+            titre="Cuisine"
+            lien="/test.html"
+            image="/public/images/img_cuisine_cat.jpg"
+            id="ClassCooking"
+  />
+  <CatSection
+            titre="Couture"
+            lien="/test.html"
+            image="/public/images/img_couture_cat.jpg"
+            id="ClassSewing"
+  />
+  <CatSection
+            titre="Peinture"
+            lien="/test.html"
+            image="/public/images/img_peinture_cat.jpg"
+            id="ClassPainting"
+  />
+  <CatSection
+            titre="Bijoux"
+            lien="/test.html"
+            image="/public/images/img_bijoux_cat.jpg"
+            id="ClassJewelry"
+  />
+    </div>
+</div>
     </>
-  )
+)
 }
 
-export default App
+export default App;
---
+++ src/Componant/CategoriesSection.jsx
@@ -0,0 +1,22 @@
+import './Categoriesort.css'
+
+function CatSection({titre, lien, image, id}) {
+    return(
+
+    //Pour pouvoir mettre plein d'element wooow (je parle de ça <>)
+    <>
+    <button
+ 
+    class="ClassSort"
+    id={id}
+    onClick = {() => window.location.href = lien}
+    >
+    <span>{titre}</span>
+    <img src={image} alt={`image de ${titre}`}/>
+
+    </button>
+    </>
+    );
+}
+
+export default CatSection;
