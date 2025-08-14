66db356a149f9c13e009f9dd42f3c104e8e1ba21
itigra5 <ilhem.mezaache22@gmail.com>
2025-08-14 19:34:46 +0200

    Ajout mini db pour test

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,40 +2,16 @@ import React from "react";
 import { useState } from "react";
 import MiniLising from "../Components/MiniLising";
 import '../Components/Minilising.css';
+import myData from '../Data.json';
 
 function Home() {
-    const [annonces] = useState([
-        {
-            id :1,
-            title : "Pancakes",
-            user : "Tamer",
-            image : "https://www.tasteofhome.com/wp-content/uploads/2024/09/Buttermilk-Pancakes_EXPS_TOHVP24_33639_MF_08_15_1_RMS.jpg?fit=300,300&webp=1"
-        },
-        {
-            id :2,
-            title : "crepes",
-            user : "Jean?",
-            image : "https://sallysbakingaddiction.com/wp-content/uploads/2023/06/homemade-crepes-1-1024x1536.jpg"
-        },
-        {
-            id :3,
-            title : "Pancakes",
-            user : "Tamer",
-            image : "https://www.tasteofhome.com/wp-content/uploads/2024/09/Buttermilk-Pancakes_EXPS_TOHVP24_33639_MF_08_15_1_RMS.jpg?fit=300,300&webp=1"
-        },
-        {
-            id :4,
-            title : "crepes",
-            user : "Jean?",
-            image : "https://sallysbakingaddiction.com/wp-content/uploads/2023/06/homemade-crepes-1-1024x1536.jpg"
-        }
-    ]);
+
     return(
         <>
         <h1 class="Title">Autour de vous</h1>
         {/* Curly braces to insert JS*/}
         <div class="tktfrr">
-                {annonces.map(a => (
+                {myData.annonces.map(a => (
                     <MiniLising
                     key={a.id}
                     title={a.title}
@@ -47,7 +23,7 @@ function Home() {
         </div>
         <h1 class="Title">Recomendé pour vous</h1>
         <div class="tktfrr">
-                {annonces.map(a => (
+                {myData.annonces.map(a => (
                     <MiniLising
                     key={a.id}
                     title={a.title}
