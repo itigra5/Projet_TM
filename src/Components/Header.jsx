4a9c80ff231c110616477858cd83f724fcb0dc96
itigra5 <ilhem.mezaache@gmail.com>
2025-08-30 19:22:09 +0200

    Detaills

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import './Layout.css'
 function Header() {
     return(
     <div id="header"> 
-        <span class="head" id="LogoSite">Logo / </span>
-        <span class="head" id="PageName">Khotek</span>
+        <span class="head" id="LogoSite"> </span>
+        <span class="head" id="PageName">Yucrea</span>
     </div>
     );
 }
