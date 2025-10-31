fb55cfc185a4ded58694cec10a83aba153cd6719
giuliafelli18-lie <giulia.felli08@gmail.com>
2025-10-31 19:20:16 +0100

    Modification du front

--- src/Pages/ScrollingPage.jsx
+++ src/Pages/ScrollingPage.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import AnnonceSwip from "../Components/AnnonceSwip";
 
-
-function Scrolling(){
-    return(
-        <div class="scrollPage">
-            <AnnonceSwip/>
-            <AnnonceSwip/>
-            <AnnonceSwip/>
-        </div>
-    );
+function Scrolling() {
+  return (
+    <div className="scrollPage">
+      <section className="Slide"><AnnonceSwip /></section>
+      <section className="Slide"><AnnonceSwip /></section>
+      <section className="Slide"><AnnonceSwip /></section>
+    </div>
+  );
 }
 
 export default Scrolling;
